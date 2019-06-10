03c1115becaa52dc67d5ddfd80092a97b79de3bc
Rishabh Singhvi <rdpsinghvi@gmail.com>
2019-06-10 17:50:57 -0400

    removed redundant files

--- website.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,current_app, url_for
-
-app = Flask(__name__, static_url_path='/static')
-
-@app.route('/')
-def index():
-    return current_app.send_static_file('index.html')
-
-
