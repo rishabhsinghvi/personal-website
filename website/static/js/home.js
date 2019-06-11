48a33de6d7987ac4396b81953508194226439694
Rishabh Singhvi <rdpsinghvi@gmail.com>
2019-06-10 22:58:14 -0400

    updated home.js

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -1,15 +1,44 @@
-// Wrap every letter in a span
+var links = document.querySelectorAll('.ext_link');
+
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+
 $('.text-intro .letters').each(function(){
     $(this).html($(this).text().replace(/([^\x00-\x80]|\w|,|')/g, "<span class='letter'>$&</span>"));
   });
   
-  anime.timeline()
+anime.timeline()
     .add({
       targets: '.text-intro .letter',
-      translateY: ["1.1em", 0],
+      translateY: ["1.2em", 0],
       translateZ: 0,
       duration: 1500,
       delay: function(el, i) {
         return 50 * i;
       }
+    })
+    .add({
+      targets: links,
+      translateX: [-width, 0],
+      delay: function(el, i, t){
+        return i*100;
+      },
+      endDelay: function(el, i, t)
+      {
+        return (t-i)*100;
+      },
+      duration: 2000
     });
+
+
+function hover(el)
+    {}
+
+function unhover(el)
+    {}
