ec066c25cbb3fed850e520a48cbfe0061c30c95f
Rishabh Singhvi <rdpsinghvi@gmail.com>
2019-06-11 00:37:21 -0400

    changed gitignore

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask,current_app, url_for
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from website import utils
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = utils.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = utils.SQL_DB_PATH
+db = SQLAlchemy(app)
 
 from website import routes
--- website/templates/message_model.py
+++ website/templates/message_model.py
@@ -1,1 +1,12 @@
-from flask import Model
+from website import db
+
+class Message(db.Model):
+    m_id = db.Column(db.Integer, primary_key = True)
+    m_name = db.Column(db.String(20), nullable = False)
+    m_email = db.Column(db.String(100), nullable = False)
+    m_msg = db.Column(Text, nullable = False)
+    m_msg_time = db.Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Message({self.m_name}, {self.m_email}, {self.m_msg})"
+    
