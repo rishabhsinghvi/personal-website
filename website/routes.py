59cf3a9b48e541ed7fb05795c5c09d8a80bae0cb
Rishabh Singhvi <rdpsinghvi@gmail.com>
2020-04-09 02:47:13 -0400

    final change

--- website/routes.py
+++ website/routes.py
@@ -7,20 +7,3 @@ from flask import render_template, redirect, url_for, request
 def index():
     return render_template('index.html')
 
-
-@app.route('/about', methods=['GET'])
-@app.route('/about.html', methods=['GET'])
-def about():
-    return render_template('about.html')
-
-
-@app.route('/skills', methods=['GET'])
-@app.route('/skills.html', methods=['GET'])
-def skills():
-    return render_template('skills.html')
-
-
-@app.route('/contact', methods=['GET'])
-@app.route('/contact.html', methods=['GET'])
-def contact():
-    return render_template('contact.html')
